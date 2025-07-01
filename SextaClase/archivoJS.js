7d8e139db88dd42d631307b6e97a88c354f0afc8
nico ryser <nryser@student.42lausanne.ch>
2025-07-01 13:57:47 +0200

    6clase

---
+++ SextaClase/archivoJS.js
@@ -0,0 +1,14 @@
+
+var nombre ="Horacio";
+var edad = 25;
+var a = 5;
+var b = 22;
+var suma = a + b ;
+var texto = "5+22";
+alert("Mi nombre es :" + " " + nombre + " mi edad es: "+ edad + " años");
+console.log("La suma es: " + suma + "<br>");
+a = 10;
+b = 45;    
+suma = a + b;  
+console.log("La suma es: " + suma + "<br>");
+ 
